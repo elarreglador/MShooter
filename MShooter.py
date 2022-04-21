e02dd546f93b3ddc5ec38427e252bd5ac430a553
David M <elarreglador@protonmail.com>
2022-04-21 22:28:54 +0200

    Funcion var2matrix

--- MShooter.py
+++ MShooter.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 import os
+import random
 
-# función asteriscos
+# función asteriscos: Encuadra un texto
 def asteriscos(texto):
 	print("**************************************************")
 	print()
@@ -10,25 +11,51 @@ def asteriscos(texto):
 	print()
 	print("**************************************************")	
 	
-# funcion pause
+# funcion pause: espera que pulses Intro
 def pausa():
 	input("\nPulsa Enter para continuar...")	
 	
-# funcion cls
+# funcion cls: borra la pantalla
 def cls():
 	if os.name == "posix":
 		os.system ("clear")
 	elif os.name == "ce" or os.name == "nt" or os.name == "dos":
 		os.system ("cls")
 		
-# funcion lee
+# funcion lee: devuelve el contenido de un archivo
 def lee(archivo):
 	with open(archivo, 'r') as file:
 		contenido = file.read().replace('\n', '')
 		return contenido
+		
+# funcion var2matrix: convierte una variable separada con ; a matriz
+def var2matrix(var):
+	varSplit = var.split(';')
+	return varSplit
 
 #MAIN
 cls()
 asteriscos ("Mecanography Shooter\n By El Arreglador")
 pausa()
-print(lee("./lvl/01.txt"))
+cls()
+
+matriz = var2matrix(lee("./lvl/01.txt"))
+numero=10
+while numero >= 1:
+	lenMatriz=len(matriz)-1
+	objetivo = (matriz[random.randint(0,lenMatriz)])
+	print (objetivo)
+	numero = numero - 1
+	
+
+
+
+#print (matriz)
+#print("Matriz tiene: " + str(len(matriz)) + " elementos.")
+
+
+
+
+
+
+
