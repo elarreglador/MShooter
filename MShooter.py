2fa012a7272e44efa56fd76f8b0b8ec1438e775a
David M <elarreglador@protonmail.com>
2022-04-21 21:00:32 +0200

    Funcion lee

--- MShooter.py
+++ MShooter.py
@@ -20,8 +20,15 @@ def cls():
 		os.system ("clear")
 	elif os.name == "ce" or os.name == "nt" or os.name == "dos":
 		os.system ("cls")
+		
+# funcion lee
+def lee(archivo):
+	with open(archivo, 'r') as file:
+		contenido = file.read().replace('\n', '')
+		return contenido
 
 #MAIN
 cls()
 asteriscos ("Mecanography Shooter\n By El Arreglador")
 pausa()
+print(lee("./lvl/01.txt"))
